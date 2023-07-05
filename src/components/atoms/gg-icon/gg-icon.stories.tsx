4e77b0fe59188d6637934aa2b8a9b340ac8ad340
gulagha <gulaghagozalov@gmail.com>
2023-07-05 18:25:38 +0300

    Add icons
    Icons can be used by name attribute

---
+++ src/components/atoms/gg-icon/gg-icon.stories.tsx
@@ -0,0 +1,49 @@
+export default {
+  title: 'Example/Icon',
+  tags: ['autodocs'],
+  render: () => {
+    return `
+      <gg-icon name='delete-icon'> </gg-icon>
+      <gg-icon name='light-mode-icon'> </gg-icon>
+      <gg-icon name='dark-mode-icon'> </gg-icon>
+      <gg-icon name='edit-icon'> </gg-icon>
+    `
+  }
+};
+
+export const DarkMode = {
+  decorators: [(Story) => {
+    const wrapper = document.createElement('div');
+    wrapper.setAttribute('data-theme', 'dark');
+    wrapper.innerHTML = Story();
+    return wrapper;
+  }],
+  parameters: {
+    backgrounds: {
+      default: 'dark',
+      values: [
+        {
+          name: 'dark',
+          value: '#000000',
+        },
+      ],
+    },
+  },
+};
+
+export const LightMode = {
+  args: {
+    name: 'light-mode-icon'
+  },
+  parameters: {
+    backgrounds: {
+      default: 'light',
+      values: [
+        {
+          name: 'light',
+          value: '#ffffff',
+        },
+      ],
+    },
+  },
+};
