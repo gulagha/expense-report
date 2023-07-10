4dfe928816ad2f04bee6ad9d38df32d27fd68f20
gulagha <gulaghagozalov@gmail.com>
2023-07-11 01:55:16 +0300

    Configure global css for main app

--- stencil.config.ts
+++ stencil.config.ts
@@ -2,6 +2,7 @@ import { Config } from '@stencil/core';
 
 export const config: Config = {
   namespace: 'expense-report',
+  globalStyle: 'src/globals.css',
   outputTargets: [
     {
       type: 'dist',
