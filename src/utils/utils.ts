cb41ebfd933712ffa44569b14b4affa5337f5c94
gulagha <gulaghagozalov@gmail.com>
2023-07-10 19:33:35 +0300

    Add fake data and fetch function

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -24,3 +24,85 @@ export function groupByMonth(data: ExpenseData[]): GroupedExpense[] {
   return Object.values(expenses);
 }
 
+const fakeData = [
+  {
+    "expenseId": "ASFAS",
+    "amount": 10.50,
+    "date": 1625526000000,
+    "description": "Grocery shopping at the local supermarket"
+  },
+  {
+    "expenseId": "ASFASs",
+    "amount": 25.00,
+    "date": 1625612400000,
+    "description": "Dinner at a cozy Italian restaurant"
+  },
+  {
+    "expenseId": "ASFAsS",
+    "amount": 10.50,
+    "date": 1625526000000,
+    "description": "Grocery shopping at the local supermarket"
+  },
+  {
+    "expenseId": "ASFsdAS",
+    "amount": 25.00,
+    "date": 1643727600000,
+    "description": "Dinner at a cozy Italian restaurant"
+  },
+  {
+    "expenseId": "ASFAS",
+    "amount": 10.50,
+    "date": 1625526000000,
+    "description": "Grocery shopping at the local supermarket"
+  },
+  {
+    "expenseId": "ASFASs",
+    "amount": 25.00,
+    "date": 1625612400000,
+    "description": "Dinner at a cozy Italian restaurant"
+  },
+  {
+    "expenseId": "ASFAsS",
+    "amount": 10.50,
+    "date": 1625526000000,
+    "description": "Grocery shopping at the local supermarket"
+  },
+  {
+    "expenseId": "ASFsdAS",
+    "amount": 25.00,
+    "date": 1643727600000,
+    "description": "Dinner at a cozy Italian restaurant"
+  },
+  {
+    "expenseId": "ASFAS",
+    "amount": 10.50,
+    "date": 1625526000000,
+    "description": "Grocery shopping at the local supermarket"
+  },
+  {
+    "expenseId": "ASFASs",
+    "amount": 25.00,
+    "date": 1625612400000,
+    "description": "Dinner at a cozy Italian restaurant"
+  },
+  {
+    "expenseId": "ASFAsS",
+    "amount": 10.50,
+    "date": 1625526000000,
+    "description": "Grocery shopping at the local supermarket"
+  },
+  {
+    "expenseId": "ASFsdAS",
+    "amount": 25.00,
+    "date": 1643727600000,
+    "description": "Dinner at a cozy Italian restaurant"
+  },
+];
+
+export function fetchExpenses() {
+  return new Promise<ExpenseData[]>((resolve) => {
+    setTimeout(() => {
+      resolve(fakeData);
+    }, 1000);
+  });
+}
